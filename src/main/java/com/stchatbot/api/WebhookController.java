dbc9a7cf16783436051c8c95fdc1bc04b0afde9c
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-20 22:46:55 +0900

    set webhook

---
+++ src/main/java/com/stchatbot/api/WebhookController.java
@@ -0,0 +1,17 @@
+package com.stchatbot.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebhookController {
+    @GetMapping("/health")
+    public String health() {
+        return "health check!";
+    }
+    @PostMapping("/webhook")
+    public void webhook() {
+
+    }
+}

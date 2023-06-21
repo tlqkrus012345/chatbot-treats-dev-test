7a80b70eb75ff67f3034014ec8699bc99390f933
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-22 00:04:58 +0900

    set webhook test

--- src/main/java/com/stchatbot/api/WebhookController.java
+++ src/main/java/com/stchatbot/api/WebhookController.java
@@ -18,10 +18,8 @@ public class WebhookController {
 
     }
     @PostMapping("/api")
-    public void webhook2(@RequestBody WebhookResponse webhookResponse, @RequestBody WebhookCallbackData webhookCallbackData) {
+    public void webhook2(@RequestBody WebhookCallbackData webhookCallbackData) {
         log.info("post");
-        log.info(String.valueOf(webhookResponse.getStatus()));
-        log.info(webhookResponse.getStatusMessage());
         log.info(webhookCallbackData.getEventType());
     }
 }

097fd20439ca55fca2477857c05dd4226366d592
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-22 11:11:13 +0900

    set webhook test

--- src/main/java/com/stchatbot/config/WebhookConfig.java
+++ src/main/java/com/stchatbot/config/WebhookConfig.java
@@ -3,6 +3,8 @@ package com.stchatbot.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -13,16 +15,15 @@ import javax.annotation.PreDestroy;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class WebhookConfig {
+public class WebhookConfig implements ApplicationRunner {
 
     private final RestTemplate restTemplate;
 
     private String token = "512fc36a9467e3c8-83dc1ff986f69070-ecdb66a74e9c42c6";
     private String url = "https://chatapi.viber.com/pa/set_webhook";
     private String payload = "{\"url\":\"https://stchatbot.site/api\"}";
-    @PostConstruct
-    public void setWebhook() {
-
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         httpHeaders.set("X-Viber-Auth-Token", token);
@@ -30,12 +31,6 @@ public class WebhookConfig {
         HttpEntity<String> httpEntity = new HttpEntity<>(payload, httpHeaders);
         ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
 
-        if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            log.info("Webhook Response: {}", responseEntity.getBody());
-        } else {
-            log.info("Webhook Fail");
-        }
+        log.info(responseEntity.getBody());
     }
-
-
 }

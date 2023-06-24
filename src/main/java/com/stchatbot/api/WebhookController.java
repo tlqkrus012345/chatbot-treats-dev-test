93a003e8c3a231bb5565d123c5cc22d92473c32c
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-24 14:58:48 +0900

    set webhook test

--- src/main/java/com/stchatbot/api/WebhookController.java
+++ src/main/java/com/stchatbot/api/WebhookController.java
@@ -2,6 +2,7 @@ package com.stchatbot.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,11 @@ public class WebhookController {
     @PostMapping("/")
     public void webhook1(@RequestBody WebhookCallbackData webhookCallbackData) {
         log.info("post");
+        log.info(webhookCallbackData.getEventType());
+    }
+    @GetMapping("/")
+    public void webhook2(@RequestBody WebhookCallbackData webhookCallbackData) {
+        log.info("get");
         log.info(webhookCallbackData.getEventType());
     }
 }

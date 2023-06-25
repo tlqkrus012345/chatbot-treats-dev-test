9c81ccafbb8acf80d74c014357b68bd6c1c1ec11
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-25 18:27:26 +0900

    conversation test

--- src/main/java/com/stchatbot/s3/AwsS3.java
+++ src/main/java/com/stchatbot/s3/AwsS3.java
@@ -4,19 +4,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class AwsS3 {
-    private String key;
-    private String path;
-
-    public AwsS3() {
-
-    }
-
-    @Builder
-    public AwsS3(String key, String path) {
-        this.key = key;
-        this.path = path;
-    }
-}
+//@Getter
+//@Setter
+//public class AwsS3 {
+//    private String key;
+//    private String path;
+//
+//    public AwsS3() {
+//
+//    }
+//
+//    @Builder
+//    public AwsS3(String key, String path) {
+//        this.key = key;
+//        this.path = path;
+//    }
+//}

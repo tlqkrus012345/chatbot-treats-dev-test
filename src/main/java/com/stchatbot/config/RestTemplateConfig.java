22985f7331bd0ef441c8fcad4d9c71fc0761d995
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-25 14:02:20 +0900

    set webhook test

--- src/main/java/com/stchatbot/config/RestTemplateConfig.java
+++ src/main/java/com/stchatbot/config/RestTemplateConfig.java
@@ -1,8 +1,17 @@
 package com.stchatbot.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
@@ -19,6 +28,22 @@ public class RestTemplateConfig {
 //    }
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .additionalInterceptors(clientHttpRequestInterceptor())
+                .build();
     }
+    public ClientHttpRequestInterceptor clientHttpRequestInterceptor() {
+        return ((request, body, execution) -> {
+            RetryTemplate retryTemplate = new RetryTemplate();
+            retryTemplate.setRetryPolicy(new SimpleRetryPolicy(3));
+            try {
+                return retryTemplate.execute(context -> execution.execute(request, body));
+            } catch (Throwable throwable) {
+                throw new RuntimeException(throwable);
+            }
+        });
+    }
+
 }
--- src/main/java/com/stchatbot/config/WebhookConfigV2.java
+++ src/main/java/com/stchatbot/config/WebhookConfigV2.java
@@ -2,8 +2,10 @@ package com.stchatbot.config;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpEntity;
@@ -18,6 +20,7 @@ import java.util.Map;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebhookConfigV2 {
     private String token = "513b21592f67e244-924f73242a636229-b8f631acb00784c";
     private String webhookUrl = "https://chatapi.viber.com/pa/set_webhook";
@@ -25,21 +28,20 @@ public class WebhookConfigV2 {
     private String payload1 = "https://4f68-1-227-43-68.ngrok-free.app";
     String body1 = "{\"url\":\"https://552a-1-227-43-68.ngrok-free.app/api\",\"event_type\":[\"delivered\"]|}";
 
+    private final RestTemplateConfig restTemplateConfig;
     @EventListener(ApplicationReadyEvent.class)
     public void setwebhook(ApplicationReadyEvent event) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        RestTemplate restTemplate = new RestTemplate();
-
         String body = objectMapper.writeValueAsString(createWebHookParams());
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("X-Viber-Auth-Token", token);
         HttpEntity<String> httpEntity = new HttpEntity<>(body, httpHeaders);
-//
-        ResponseEntity<String> response = restTemplate.exchange(webhookUrl, HttpMethod.POST, httpEntity, String.class);
+
+        ResponseEntity<String> response = restTemplateConfig.restTemplate().exchange(webhookUrl, HttpMethod.POST, httpEntity, String.class);
 
         log.info("Viber Webhook status {}, body {}", response.getStatusCode(), response.getBody());
-        //
+
     }
 
     private Map<String, Object> createWebHookParams() {

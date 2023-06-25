6267cad9a910449fc2d6ff8b379b4961ca4f7a24
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-25 20:13:21 +0900

    conversation test

--- src/main/java/com/stchatbot/config/RestTemplateConfig.java
+++ src/main/java/com/stchatbot/config/RestTemplateConfig.java
@@ -1,17 +1,22 @@
 package com.stchatbot.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.*;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Configuration
 public class RestTemplateConfig {
@@ -26,13 +31,13 @@ public class RestTemplateConfig {
 //                .setConnectionManager(connectionManager)
 //                .build();
 //    }
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplateBuilder().build();
+//    }
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplateBuilder().build();
-    }
-
-    //@Bean
-    public RestTemplate restTemplate1() {
         return new RestTemplateBuilder()
                 .setConnectTimeout(Duration.ofSeconds(5))
                 .setReadTimeout(Duration.ofSeconds(5))
@@ -49,6 +54,35 @@ public class RestTemplateConfig {
                 throw new RuntimeException(throwable);
             }
         });
+    }
+    @Slf4j
+    static class LoggingInterceptor implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest req, byte[] body, ClientHttpRequestExecution ex) throws IOException {
+            final String sessionNumber = makeSessionNumber();
+            printRequest(sessionNumber, req, body);
+            ClientHttpResponse response = ex.execute(req, body);
+            printResponse(sessionNumber, response);
+            return response;
+        }
+
+        private String makeSessionNumber() {
+            return Integer.toString((int) (Math.random() * 1000000));
+        }
+
+        private void printRequest(final String sessionNumber, final HttpRequest req, final byte[] body) {
+            log.info("[{}] URI: {}, Method: {}, Headers:{}, Body:{} ",
+                    sessionNumber, req.getURI(), req.getMethod(), req.getHeaders(), new String(body, StandardCharsets.UTF_8));
+        }
+
+        private void printResponse(final String sessionNumber, final ClientHttpResponse res) throws IOException {
+            String body = new BufferedReader(new InputStreamReader(res.getBody(), StandardCharsets.UTF_8)).lines()
+                    .collect(Collectors.joining("\n"));
+
+            log.info("[{}] Status: {}, Headers:{}, Body:{} ",
+                    sessionNumber, res.getStatusCode(), res.getHeaders(), body);
+        }
     }
 
 }

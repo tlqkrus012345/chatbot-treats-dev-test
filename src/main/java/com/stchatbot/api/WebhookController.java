159c3a33bc1f4d1bbcb54dd943faa2cb3e20aad0
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-25 21:18:58 +0900

    conversation test

--- src/main/java/com/stchatbot/api/WebhookController.java
+++ src/main/java/com/stchatbot/api/WebhookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class WebhookController {
-    @GetMapping("/health")
+    @GetMapping("/")
     public String health() {
         return "health check";
     }

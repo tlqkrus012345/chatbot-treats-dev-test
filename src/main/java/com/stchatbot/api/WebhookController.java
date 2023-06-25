8c741849b06bace27d626742b4a6239f26115b1d
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-25 17:28:40 +0900

    conversation test

--- src/main/java/com/stchatbot/api/WebhookController.java
+++ src/main/java/com/stchatbot/api/WebhookController.java
@@ -1,6 +1,7 @@
 package com.stchatbot.api;
 
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebhookController {
     @GetMapping("/health")
-    public ResponseEntity<String> health() {
-        return ResponseEntity.ok("health check");
-    }
+    public void health() {}
     @PostMapping("/")
-    public void webhook1(@RequestBody WebhookCallbackData webhookCallbackData) {
-        log.info("post");
-        log.info(webhookCallbackData.getEventType());
+    public void webhook1(@RequestBody String callback) {
+        JSONObject jsonObject = new JSONObject(callback);
+        String eventType = jsonObject.getString("event");
+        if (eventType.equals("conversation_started")) {
+            // user의 name을 추출
+            JSONObject user = jsonObject.getJSONObject("user");
+            String name = user.getString("name");
+            log.info(name);
+        }
+
     }
-    @GetMapping("/")
+    //@GetMapping("/")
     public void webhook2(@RequestBody WebhookCallbackData webhookCallbackData) {
         log.info("get");
         log.info(webhookCallbackData.getEventType());

db57673fb2eea745504c1c9effc58fae0fcab658
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-26 23:57:00 +0900

    s3 test

--- src/main/java/com/stchatbot/ServerApplication.java
+++ src/main/java/com/stchatbot/ServerApplication.java
@@ -12,9 +12,6 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 @SpringBootApplication
 public class ServerApplication {
-	static {
-		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 		log.info("==============main=========");
